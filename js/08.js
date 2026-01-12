6aab450625e675495a3968791a0ea8801b5a8864
abelbd-96 <abel.bd96@gmail.com>
2026-01-12 11:38:51 -0600

    Add 08.js with notes about arrays in JavaScript and update index.html to reference it

---
+++ js/08.js
@@ -0,0 +1,10 @@
+/* ========== 8. Arrays ========== */
+// A differece between arrays and objects is that arrays don't use named keys, they use numbered indexes.
+// The first element in an array is at index 0, the second at index 1, and so on.
+// You can create an array using square brackets [] and separate the elements with commas.
+const technologies = [20, 30, 40];
+console.table(technologies); // [20, 30, 40]
+// You can access elements in an array using their index.
+console.log(technologies[0]); // 20
+console.log(technologies[1]); // 30
+console.log(technologies[2]); // 40
